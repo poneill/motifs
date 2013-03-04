6fc96172c3e2435a4acdc3905036dbf418ab4f7c
Patrick <synapse.and.syntax@gmail.com>
2013-03-04 18:41:42 -0500

    init commit

---
+++ motifs.py
@@ -0,0 +1,59 @@
+"""Turn Prodoric into a usable Python module."""
+#Code could stand some cleaning, but is usable.
+
+import os,csv
+from utils import *
+from collections import namedtuple
+BindingSiteRecord = namedtuple("BindingSiteRecord",["genome", "tf", "operon",
+                                                    "strand", "sequence",
+                                                    "startpos", "endpos",
+                                                    "rflank", "lflank",
+                                                    "regulation"])
+
+class BindingSite(str):
+    # Basically subclass string for TFBS attributes
+    def __new__(cls,genome,tf,operon,strand,sequence,startpos,endpos,rflank,lflank):
+        # YANETUT:
+        # http://stackoverflow.com/questions/820742/how-to-bestow-string-ness-on-my-class
+        newobj = str.__new__(cls,sequence)
+        newobj.genome = genome
+        newobj.tf = tf
+        newobj.operon= operon
+        newobj.strand = strand
+        newobj.startpos = startpos
+        newobj.endpos = endpos
+        newobj.lflank = lflank
+        newobj.rflank = rflank
+        return newobj
+
+class Organism(object):
+    pass
+
+csv_names = [f for f in os.listdir('/home/poneill/binding_sites') #make robust later
+        if f.endswith(".csv") and not f == "dump.csv"]
+
+for csv_name in csv_names:
+    org_name = "_".join(csv_name.split(" ")[:2])
+    file_name = os.path.join("/home/poneill/binding_sites",csv_name)
+    if org_name.endswith("."):
+        org_name = org_name[:-1]
+    #omit header
+    records = map(BindingSiteRecord._make, csv.reader(open(file_name,'rb')))[1:]
+    binding_sites = [BindingSite(record.genome,record.tf,record.operon,
+                                 record.strand,record.sequence,record.startpos,
+                                 record.endpos,record.rflank,record.lflank)
+                     for record in records]
+    tf_names = set(bs.tf for bs in binding_sites)
+    exec("%s = Organism()" % org_name)
+    for tf_name in tf_names:
+        tf_binding_sites = [bs for bs in binding_sites if bs.tf == tf_name]
+        setattr(eval(org_name),tf_name,tf_binding_sites)
+        #exec_string = ("%s.%s = %s" %
+        #(org_name,tf_name,tf_binding_sites))
+        # exec(exec_string)
+
+        #This is perhaps the most subtle bug I have ever encountered.
+        #I retain it here for posterity.  Never use exec when there's
+        #another option.
+        
+        
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='motifs',
+      version='0.1',
+      py_modules=['motifs'],
+      )
